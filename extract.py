6c59cae46f96b765c618f3c54a0bf97bbe0b9365
Conor-Doyle-29 <104837327+Conor-Doyle-29@users.noreply.github.com>
2022-12-05 12:09:52 +0000

    update

--- extract.py
+++ extract.py
@@ -1,3 +1,5 @@
+import statistics as stat
+
 file=open("tmp.csv","r")
 dataIn=file.read()
 file.close
@@ -10,8 +12,36 @@ print(tmpList)
 list1=tmpList[0::3]
 print(list1)
 
+list1=[int(item) for item in list1]
+
 list2=tmpList[1::3]
 print(list2)
 
+list2=[int(item) for item in list2]
+
 list3=tmpList[2::3]
 print(list3)
+
+list3=[int(item) for item in list3]
+
+mean1= stat.mean(list1)
+print(mean1)
+
+mean2= stat.mean(list2)
+print(mean2)
+
+mean3= stat.mean(list3)
+print(mean3)
+
+median1 = stat.median(list1)
+print(median1)
+
+median2 = stat.median(list2)
+print(median2)
+
+
+median3 = stat.median(list3)
+print(median3)
+
+
+
